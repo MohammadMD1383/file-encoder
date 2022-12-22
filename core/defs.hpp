5573b95b19f73f35d80b1129eaf31f175463519c
Mohammad Mostafa <ezmmd83@gmail.com>
2022-12-22 20:11:55 +0330

    added verbosity check for printing logs

--- core/defs.hpp
+++ core/defs.hpp
@@ -1,10 +1,12 @@
 #ifndef FILE_ENCODER_DEFS_HPP
 #define FILE_ENCODER_DEFS_HPP
 
+static char const * const VERBOSE = std::getenv("VERBOSE");
+
 //region output helpers
-#define INFO(msg)    std::cerr << "\033[46;97m" "[INFO]:"    "\033[0;36m " msg "\033[0m" << std::endl
-#define WARNING(msg) std::cerr << "\033[43;97m" "[WARNING]:" "\033[0;33m " msg "\033[0m" << std::endl
-#define ERROR(msg)   std::cerr << "\033[41;97m" "[ERROR]:"   "\033[0;31m " msg "\033[0m" << std::endl
+#define INFO(msg)    if (VERBOSE && std::strcmp(VERBOSE, "1") == 0) std::cerr << "\033[46;97m" "[INFO]:"    "\033[0;36m " msg "\033[0m" << std::endl
+#define WARNING(msg) if (VERBOSE && std::strcmp(VERBOSE, "1") == 0) std::cerr << "\033[43;97m" "[WARNING]:" "\033[0;33m " msg "\033[0m" << std::endl
+#define ERROR(msg)   if (VERBOSE && std::strcmp(VERBOSE, "1") == 0) std::cerr << "\033[41;97m" "[ERROR]:"   "\033[0;31m " msg "\033[0m" << std::endl
 //endregion
 
 //region exit codes
